2e0ebe998eb4ca1c0e5128d03b6786bdc6dbb720
William Ray Johnson <willrayjohnson@gmail.com>
2018-01-28 21:28:28 -0600

    Finished up main() logic, added the rest of the genki verbs

--- randVocab.py
+++ randVocab.py
@@ -1,7 +1,12 @@
+#!/usr/bin/python3
+
 '''
 A script that prints random japanese vocab words from a file and prints
 them to screen.
 '''
+
+import numpy as np
+import os
 
 def getVocab(filename):
     vocab = []
@@ -17,9 +22,13 @@ def main():
     vocabFilename = "genkiVerbs.txt"
 
     vocab = getVocab(vocabFilename)
+    vocab = np.random.choice(vocab, len(vocab), replace=False)
 
     for word in vocab:
+        os.system('cls' if os.name == 'nt' else 'clear')
         print(word)
+        input()
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 if __name__ == '__main__':
     main()
